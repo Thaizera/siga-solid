44912f47dee6f5e624604826f615b0c69b6ab909
Thais <thaislemos214@gmail.com>
2026-09-02 21:57:11 -0300

    envio do e-mail

---
+++ src/siga/ServicoEmail.java
@@ -0,0 +1,9 @@
+package siga;
+
+public class ServicoEmail {
+
+    // Cuida apenas do envio do e-mail
+    public void enviar(String destinatario, String conteudo) {
+        System.out.println("Enviando e-mail para " + destinatario + "...");
+    }
+}
